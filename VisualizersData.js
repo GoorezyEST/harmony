a46452bd5a4633323a5de48307185aa8f0975780
GoorezyEST <goorezyestudios@gmail.com>
2024-03-17 18:56:19 -0300

    Modified images

--- VisualizersData.js
+++ VisualizersData.js
@@ -1,22 +1,22 @@
 export const VisualizersData = [
   {
     name: "TrailsVisualizer",
-    img: "https://i.imgur.com/cSdarZX.png",
+    img: "https://i.imgur.com/Aua3ZBi.png",
   },
   {
     name: "LeafsVisualizer",
-    img: "https://i.imgur.com/dRtmeNj.png",
+    img: "https://i.imgur.com/9kz7XfQ.png",
   },
   {
     name: "ParticlesVisualizer",
-    img: "https://i.imgur.com/iT9jfzm.png",
+    img: "https://i.imgur.com/Vyh2u0T.png",
   },
   {
     name: "LinesVisualizer",
-    img: "https://i.imgur.com/X2oCD3l.png",
+    img: "https://i.imgur.com/EdLfs6Q.png",
   },
   {
     name: "EvzelynVisualizer",
-    img: "https://i.imgur.com/dFOZbZK.png",
+    img: "https://i.imgur.com/wSNmdBd.png",
   },
 ];
